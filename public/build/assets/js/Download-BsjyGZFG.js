b25d1943e664da60168cb7395a1539e2fb8c4065
Emam Hosen <emamhsajeeb@gmail.com>
2025-08-24 10:00:39 +0000

    ok

---
+++ public/build/assets/js/Download-BsjyGZFG.js
@@ -0,0 +1,1 @@
+import{j as o}from"./react-core-SLWS96gT.js";import{g as t}from"./mui-libraries-CQTi8RHu.js";const s=t(o.jsx("path",{d:"M5 20h14v-2H5zM19 9h-4V3H9v6H5l7 7z"}),"Download");export{s as D};
